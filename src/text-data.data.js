d2653fa449487b71769dff4127f2ddec74bef8ab
Arseni Dmitriev <arseni@Arsenis-MacBook-Pro.local>
2022-01-17 13:37:33 -0500

    added to deployment

--- src/text-data.data.js
+++ src/text-data.data.js
@@ -4,7 +4,7 @@ const TEXT_DATA = {
         title: 'Morning Aroma',
         word: 'This project required me to build a fully responsive e-commerce website with the designs I was inspired from. I used React JS as well as redux for state management. I added the back-end with Node Js and Express and the Stripe API for payment. I also used MangoDB for my database, to store user information.',
         project_background_text:"This was an e-commerce website, built with the idea of having a fully interactive shopping experience. This included the shopping cart and the payment method as well as a sign-in page. Made to be used with mobile and desktop designs in order to accommodate a shopping experience on any device. Creating this project has helped me refine my workflow and solve real-world coding problems. The component based approach of React was the ideal structure for the layout of an e-commerce website.",
-        project_resume_description:"This project required me to build a fully responsive ecommerce website with CRUD functionality incorporated in the back-end with a database. I used React and Redux for the areas that required interactivity, such as the shopping cart.",
+        project_resume_description:"This is a project that required me to build a fully responsive ecommerce website with CRUD functionality incorporated in the back-end with a database. I used React and Redux for the areas that required interactivity, such as the shopping cart.",
         project_resume_tech:["javascript","react","redux","sass","nodejs","express","mangodb","responsive"],
         project_resume_website_link:"https://elegant-hopper-860fd7.netlify.app/",
         project_resume_code_link:"https://github.com/RusssianMusketeer/Morning-Aroma",
